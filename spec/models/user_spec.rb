6a37bbbf8ee319ba3cd763e29f8b093cced21266
yutakakinjyo <yutakakinjyo@gmail.com>
2014-09-07 22:19:36 +0900

    add test pattern of User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -51,6 +51,17 @@ describe User do
       it { should be_invalid }
     end
 
+    describe 'email address with mixed case' do
+      let(:mixed_case_email) { 'Foo@ExAmPle.CoM' }
+
+      it 'should be saved as all lower-case' do
+        @user.email = mixed_case_email
+        @user.save
+        expect(@user.email).to eq mixed_case_email.downcase
+      end
+
+    end
+
   end
 
   describe 'when name is not present' do
@@ -71,12 +82,13 @@ describe User do
   describe 'when email format is invalid' do
     it 'should be invalid' do
       addresses = %w( user@foo,com user_at_foo.org example.user@foo.
-      foo@bar_baz.com foo@bar+baz.com )
+      foo@bar_baz.com foo@bar+baz.com foo@hoge..com )
       addresses.each do | invalid_address |
         @user.email = invalid_address
         expect(@user).not_to be_valid
       end
     end
+
   end
 
   describe 'when email format is valid' do
